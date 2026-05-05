3ad54a17f3bd8b7724e7be07ccf9ef955e43a917
ortizramirezstiven2707-source <ortizramirezstiven2707@gmail.com>
2026-05-04 19:23:55 -0500

    dfgsdrgfsd

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 <body>
 
 <header>
-    <h1>Historia alejandro, andres del Cálculo Integral</h1>
+    <h1>Historia alejandro, andres, nariyanke del Cálculo Integral</h1>
     <p>Desde la antigüedad hasta la actualidad</p>
 </header>
 
